bb1a0eb882c4d99acef1b66fc7576d6a57098d52
Adebayo <adebayotobiloba9@gmail.com>
2021-01-14 13:43:06 +0100

    installed express

--- myserver.js
+++ myserver.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get("/api/rates", function (req, res) {
-  
-})
+app.get("/api/rates", function (req, res) {});
 
-app.listen(3000, function)
+app.listen(3000, function () {
+  console.log("server is running");
+});
