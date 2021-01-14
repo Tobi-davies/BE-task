8c60598441e1100faed73150cf9c5fe174527727
Adebayo <adebayotobiloba9@gmail.com>
2021-01-14 16:02:34 +0100

    http request

--- myserver.js
+++ myserver.js
@@ -2,7 +2,9 @@ const express = require("express");
 
 const app = express();
 
-app.get("/api/rates", function (req, res) {});
+app.get("/api/rates", function (req, res) {
+  res.send("Hwello");
+});
 
 app.listen(3000, function () {
   console.log("server is running");
