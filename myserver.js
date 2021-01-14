6b66a90c4544d81523f995f2018db1f53fdc66d4
Adebayo <adebayotobiloba9@gmail.com>
2021-01-14 17:26:25 +0100

    sorted query

--- myserver.js
+++ myserver.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const request = require("request");
 
 const app = express();
 
+// app.get("/api/rates", function (req, res) {
+//   res.sendFile(__dirname + "/index.html");
+// });
+
 app.get("/api/rates", function (req, res) {
-  res.send("Hwello");
+  console.log(req.query);
+
+  request("https://api.exchangeratesapi.io/latest?base=USD", function (
+    error,
+    response,
+    body
+  ) {
+    console.log(body);
+  });
+  // console.log(req.path);
 });
 
 app.listen(3000, function () {
   console.log("server is running");
 });
+
+// base=CZK&currency=EUR,GBP,USD
